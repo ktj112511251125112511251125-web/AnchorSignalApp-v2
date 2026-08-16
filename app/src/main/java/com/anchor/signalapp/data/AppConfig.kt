136e414b677df4a80d3c9206bce5f00d38f08bb2
ktj112511251125112511251125-web <ktj112511251125112511251125@gmail.com>
2026-08-16 22:59:43 +0900

    Delete app directory

--- app/src/main/java/com/anchor/signalapp/data/AppConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.anchor.signalapp.data
-
-object AppConfig {
-    // GitHub raw URL 예:
-    // https://raw.githubusercontent.com/YOUR_ID/AnchorSignalApp/main/app_data/latest.json
-    const val REMOTE_JSON_URL = ""
-}
